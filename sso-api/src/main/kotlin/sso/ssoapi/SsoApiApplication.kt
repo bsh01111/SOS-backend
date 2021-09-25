19c4ecbe3c9ac8bc5357b69219cca77c0f8d59ab
bsh <bsh01111@naver.com>
2021-09-25 20:14:09 +0900

    feat: 초기 프로젝트 세팅
    Signed-off-by: bsh <bsh01111@naver.com>

---
+++ sso-api/src/main/kotlin/sso/ssoapi/SsoApiApplication.kt
@@ -0,0 +1,11 @@
+package sso.ssoapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SsoApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SsoApiApplication>(*args)
+}
---
+++ sso-api/src/test/kotlin/sso/ssoapi/SsoApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package sso.ssoapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SsoApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
