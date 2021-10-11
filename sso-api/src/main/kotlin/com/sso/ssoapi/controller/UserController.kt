302998e939a9021a186b549ae7e9373674568dae
bsh <bsh01111@naver.com>
2021-10-11 18:45:56 +0900

    feat: cors 문제 해결
    Signed-off-by: bsh <bsh01111@naver.com>

--- sso-api/src/main/kotlin/com/sso/ssoapi/controller/UserController.kt
+++ sso-api/src/main/kotlin/com/sso/ssoapi/controller/UserController.kt
@@ -3,16 +3,24 @@ package com.sso.ssoapi.controller
 import com.sso.ssoapi.dto.ApiResponse
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 @ResponseBody
 @RequestMapping("/user")
+//@CrossOrigin(origins = ["*"], allowedHeaders = ["*"])
 class UserController {
 
     @GetMapping("/hello")
     fun hello(): ApiResponse {
         return ApiResponse("hello")
     }
+
+    @PostMapping("/login")
+    fun login(): ApiResponse {
+        return ApiResponse("hello")
+    }
+
 }
